bd0d3c3a3118026826bd299438132fda11ac4911
AxelMarouard <tinlhat63@gmail.com>
2024-12-16 14:53:51 +0100

    mise a jour du de la nav client
    mise a jour des texte et taille de police ajout du bouton pour fomulaire

--- www/navbars/navbarUtilisateur.php
+++ www/navbars/navbarUtilisateur.php
@@ -22,27 +22,27 @@ $fileName = basename($Url);
     if ($fileName=="index.php") {
     ?>
         <ul>
-            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h5 class="mb-0">Accueil</h5></div></a></li>
-            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Stages</h5></div></a></li>
-            <li><a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Animateurs</h5></div></a></li>
+            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h6 class="mb-0">ACCUEIL</h6></div></a></li>
+            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS STAGES</h6></div></a></li>
+            <li><a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS ANIMATEURS</h6></div></a></li>
         </ul>
     <?php
     }
     elseif ($fileName=="stage.php") {
         ?>
         <ul>
-            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Accueil</h5></div></a></li>
-            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h5 class="mb-0">Nos Stages</h5></div></a></li>
-            <li><a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Animateurs</h5></div></a></li>
+            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">ACCUEIL</h6></div></a></li>
+            <li><a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h6 class="mb-0">NOS STAGES</h6></div></a></li>
+            <li><a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS ANIMATEURS</h6></div></a></li>
         </ul>
         <?php
     }
     else {
     ?>
         <ul>
-            <li> <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Accueil</h5></div></a></li>
-            <li> <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Stages</h5></div></a></li>
-            <li> <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h5 class="mb-0">Nos Animateurs</h5></div></a></li>
+            <li> <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">ACCUEIL</h6></div></a></li>
+            <li> <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS STAGES</h6></div></a></li>
+            <li> <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h6 class="mb-0">NOS ANIMATEURS</h6></div></a></li>
         </ul>
     <?php
     }
@@ -54,11 +54,11 @@ $fileName = basename($Url);
 <!--Navigation web avec menu stantard-->
 <div class="container text-center mt-5 navWeb opacity-0 position-absolute">
     <div class="row justify-content-center">
-        <div class="col-12 shadow-lg p-3 mb-5 bg-body-tertiary rounded-pill d-flex justify-content-between align-items-center pe-5 ps-5">
+        <div class="col-12 shadow-lg p-2 mb-5 bg-body-tertiary rounded-pill d-flex justify-content-between align-items-center pe-5 ps-5">
             <a class="nude" href="https://www.kafeteomomes.fr">
                 <div class="d-flex  align-items-center">
-                    <img width="50" height="50" src="https://img.icons8.com/ios-filled/100/long-arrow-left.png" alt="long-arrow-left"/>
-                    <h5 class="m-0">REVENIR AU SITE WEB</h5>
+                <img width="25" height="25" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAACA0lEQVR4nO3cu2tUQRhA8SOCFjYWERF8gCQqrgoiEoIK0XRqjKV/pa9GtLcRO0Es1DS+MIkm6wMTIjcsbEREzQw4zOfO+cH0H/dk927unXtBkiRJkiRJkiRJkiRJkiRVswPY7vGv7zDwAPg+XA+B87WHatU5YAXoflmrwIXaw7VmCuj/JsbmelR7wJZMAst/idENv7521R60BWeBj1vE2Ayys/awo+4M8CEhRjc80aug08BSYow+cMwa5ZwCFhJjfAGmjVHO4C/9XUaMi8Yo5yjwNiPGJWOUjfEmMcZXYMYY5RwBXifG+AZcNkY5ExkxVoErxihnHHiVEeOqMco5BMxnxJg1RjkHgZcZMa4Zo5wDwIuMGHPGKBvjeWKMNeB6wVmatz8jxjpwo/kjVtBe4KkxYtgHPEuMMWprBbgL9PgPPxmjvD4BJ2vH2AM8CXAwuiDrfs0Yu4HHAQ5CF2it1dxHZhBiBRkY8yuLn4PcIwBP6vw4qZ8giJZ/9i4Dd4DjBOM/hgF56SQgLy4G5OX3gLxBFZC3cANyk0NAbgMKyI1yAbmVNCA3Wwfk4wgB+cDOCDzS1qs9cAtyHvq8XXvYVqR+UhZrD9qSyYQXB3wGttUetCVTW7xaY3BnThVePtP/ww7BcLdJW9EDbg7PGe+BW8aQJEmSJEmSJEmSJEmSJEniH9sA55WtLsz50PAAAAAASUVORK5CYII=" alt="left">
+                    <h6 class="m-0">REVENIR AU SITE WEB</h6>
                 </div>
             </a>
             <img class="col-lg-1 col-3" src="https://www.kafeteomomes.fr/uploads/2016/10/logo.png" alt="logo de ka fête ô môme">
@@ -66,31 +66,31 @@ $fileName = basename($Url);
             /**permet de savoir sur quelle page nous sommes sans JS*/
             if ($fileName=="index.php") {
                 ?>
-                <a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h5 class="mb-0">Accueil</h5></div></a>
-                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Stages</h5></div></a>
-                <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Animateurs</h5></div></a>
+                <a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h6 class="mb-0">ACCUEIL</h6></div></a>
+                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS STAGES</h6></div></a>
+                <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS ANIMATEURS</h6></div></a>
                 <?php
             }
             elseif ($fileName=="stage.php") {
                 ?>
-                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Accueil</h5></div></a>
-                <a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h5 class="mb-0">Nos Stages</h5></div></a>
-                <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Animateurs</h5></div></a>
+                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">ACCUEIL</h6></div></a>
+                <a class="nude" href="stageAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h6 class="mb-0">NOS STAGES</h6></div></a>
+                <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS ANIMATEURS</h6></div></a>
                 <?php
             }
             else {
                 ?>
-                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Accueil</h5></div></a>
-                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h5 class="mb-0">Nos Stages</h5></div></a>
-                <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h5 class="mb-0">Nos Animateurs</h5></div></a>
+                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">ACCUEIL</h6></div></a>
+                <a class="nude" href="stageAdmin.php"><div class="rounded-4 p-3"><h6 class="mb-0">NOS STAGES</h6></div></a>
+                <a class="nude" href="AnimateursAdmin.php"><div class="rounded-4 text-decoration-underline p-3"><h6 class="mb-0">NOS ANIMATEURS</h6></div></a>
                 <?php
             }
             ?>
-            <a href="">
-                <button>
-                    <h5>S'INCRIRE A UN STAGE</h5>
-                    <img src="" alt="">
-                </button>
+            <a class="nude" href="">
+                <div class="rounded-pill d-flex align-items-center fondJaune pe-2 ps-2 pt-3 pb-3">
+                    <h6 class="mb-0 ms-3">S'INCRIRE A UN STAGE</h6>
+                    <img width="25" height="25" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAABvUlEQVR4nO3du0okURRG4d/LmwijmbA3UvPMJgYihvoywiReMFAbgxahTUSxReyzhlofVL7pRZ2qZFcnkiRJP7Dlr8dwmOQ8yWOSRZLTJAejh5pzjPsky3fXbZJp9HBzdP5BjLfrziibtb06pj4LYpQBHr4IsvRO2ayTNYIsjbI5e6sHuFFAKsn1N6L8HT3wHBgFyChARgEyCpBRgIwCVL4S85RReMooPGUUnjIKTxmFp4zCU0bhKaPwlFF4yig8ZRSeMgpPGYWnjMJTRuEpo/CUUXjKKDxlFJ4yCk8ZhaeMwlNG4Smj8JRReMooPNNqQ2udTa7XNbw/oweeg+kbUY5HDzsXtebx9W/0oHPRSW7WCHI1etA5mDyyOCYf6v/fMbX0qxK/zxgg7Z3B0cbgaGNwtDE42hgcbQyONgZHG4OjjcHRxuBoY3C0MTjaGBxtDI42Bkcbg6ONwdHG4GhjcLQxONoYHG0MjjYGRxuDo43BYQwQY4AYA8QYIMYAMQaIMUD2/etVljPXyDh2kizc6ePYTfLsgiXLpduuLEdJHj/5uMvr3rgGRblYHV9PSU790g7nIb89eghJkqT8lhfcmbLovD1dtwAAAABJRU5ErkJggg==" alt="forward--v1">
+                </div>
             </a>
         </div>
     </div>
