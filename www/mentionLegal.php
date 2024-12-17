834b7214bc37f685bf6993b3827cada2e52d09db
AxelMarouard <tinlhat63@gmail.com>
2024-12-17 10:53:58 +0100

    Finalisation du footer avec lien a page et terme de legaliter
    finalisation du CSS et structure du footer avec créations des pages légals

---
+++ www/mentionLegal.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mention Legal</title>
+    <?php include("ressources/ressourcesCommunes.php"); ?>
+</head>
+<body>
+    <header>
+        <?php include("navbars/navbarUtilisateur.php"); ?>
+    </header>
+    <main>
+
+    </main>
+    <?php include("navbars/footer.php"); ?>
+</body>
+</html>
