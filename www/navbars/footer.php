6984ae566e5b617549a8f26d1a48717d7e86ecfc
AxelMarouard <tinlhat63@gmail.com>
2024-12-17 09:36:02 +0100

    Structure du footer

---
+++ www/navbars/footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <img src="" alt="">
+            <div>
+                <h6>PLAN DU SITE</h6>
+                <a href=""><div></div></a>
+                <a href=""><div></div></a>
+                <a href=""><div></div></a>
+                <a href=""><div></div></a>
+            </div>
+            <div>
+                <h6>DONNEES</h6>
+                <a href=""><div></div></a>
+                <a href=""><div></div></a>
+                <a href=""><div></div></a>
+            </div>
+            <div>
+                <h6>RESEAUX SOCIAUX</h6>
+                <a href=""><div></div></a>
+                <a href=""><div></div></a>
+            </div>
+        </div>
+    </div>
+</footer>
