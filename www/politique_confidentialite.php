47d565516618973f39a50b69605e3be31915519e
AxelMarouard <tinlhat63@gmail.com>
2024-12-17 14:56:30 +0100

    Mise a jour Nav et footer en responsif
    Regler le problem dans la nav
    le footer et a présent responsif
    Début de mise en page de la RGPD

---
+++ www/politique_confidentialite.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Politique confidentialité</title>
+    <?php include("ressources/ressourcesCommunes.php");?>
+</head>
+<body>
+    <header><?php include("navbars/navbarutilisateur.php");?></header>
+    <main></main>
+    <?php include("navbars/footer.php");?>
+</body>
+</html>
