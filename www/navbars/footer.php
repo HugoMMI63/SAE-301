7bdc9f9bbd3f8313c3a5fe36a5377e3c9df0d06c
AxelMarouard <tinlhat63@gmail.com>
2024-12-17 10:11:39 +0100

    mise a jour footer et creation des fichier legals

--- www/navbars/footer.php
+++ www/navbars/footer.php
@@ -1,24 +1,26 @@
 <footer>
     <div class="container">
         <div class="row">
-            <img src="" alt="">
+        <a href="index.php"><img class="col-lg-1 col-3" src="https://www.kafeteomomes.fr/uploads/2016/10/logo.png" alt="logo de ka fête ô môme"></a>
             <div>
                 <h6>PLAN DU SITE</h6>
-                <a href=""><div></div></a>
-                <a href=""><div></div></a>
-                <a href=""><div></div></a>
-                <a href=""><div></div></a>
+                <ul>
+                    <li><a href="index.php"><div>Acceuil</div></a></li>
+                    <li><a href="nos_stages.php"><div>Nos Stages</div></a></li>
+                    <li><a href=""><div>Nos Animateurs</div></a></li>
+                </ul>
             </div>
             <div>
                 <h6>DONNEES</h6>
-                <a href=""><div></div></a>
-                <a href=""><div></div></a>
-                <a href=""><div></div></a>
+                <ul>
+                    <li><a href=""><div>Menttions légales</div></a></li>
+                    <li><a href=""><div>Politique de confidentialité</div></a></li>
+                </ul>
             </div>
             <div>
                 <h6>RESEAUX SOCIAUX</h6>
-                <a href=""><div></div></a>
-                <a href=""><div></div></a>
+                <a href=""><img src="" alt=""></a>
+                <a href=""><img src="" alt=""></a>
             </div>
         </div>
     </div>
