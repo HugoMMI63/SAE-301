117ad600583b3911ca34909ee51086278fe6ed8f
AxelMarouard <tinlhat63@gmail.com>
2024-12-19 18:02:49 +0100

    Mise a jour du CSS

--- www/details_stage.php
+++ www/details_stage.php
@@ -26,7 +26,7 @@ else {
 
 // Récupérer les détails du stage via l'API
 
-$apiUrl = "http://localhost/www/API/remplacerContenuStage.php?id=".$stageId;
+$apiUrl = "http://localhost/MMI2/SAE303-GIT/SAE-301-Association/www/API/remplacerContenuStage.php?id=".$stageId;
 $response = file_get_contents($apiUrl);
 $data = json_decode($response, true);
 
@@ -40,7 +40,7 @@ $stage = $data['stage'];
 
 // Récupérer 3 stages aléatoires via l'API
 
-$apiUrl = "http://localhost/www/API/stagesAleatoires.php?id=".$stageId;
+$apiUrl = "http://localhost/MMI2/SAE303-GIT/SAE-301-Association/www/API/stagesAleatoires.php?id=".$stageId;
 $response = file_get_contents($apiUrl);
 $data = json_decode($response, true);
 
@@ -70,7 +70,7 @@ include("ressources/ressourcesCommunes.php");
         <!-- Informations du stage -->
 
             <section class="bg-light p-4 rounded shadow-sm mb-5 container-fluid text-center">
-                <h1 id="titreStage" class="text-primary text-center mb-4"><?php echo $stage[0]['titre']; ?></h1>
+                <h1 class="colorB text-center mb-4"><?php echo $stage[0]['titre']; ?></h1>
                 <img src="img/barre_separation.png" alt="Barre de séparation" class="d-block mx-auto my-3">
                 <ul class="list-unstyled">
                     <li id="categorieStage" class="mb-2"><strong>Catégorie :</strong> <?php echo $stage[0]['intitule']; ?></li>
@@ -80,8 +80,8 @@ include("ressources/ressourcesCommunes.php");
                     <li id="nbplaceStage" class="mb-2"><strong>Nombre de places :</strong> <?php echo $stage[0]['nb_places']; ?></li>
                     <li id="tarifStage" class="mb-2"><strong>Tarif :</strong> <?php echo $stage[0]['tarif_min']; ?> à <?php echo $stage[0]['tarif_max']; ?>€ selon le quotient familial</li>
                 </ul>
-                <img id="miniatureStage" src="<?php echo $stage[0]['miniature']; ?>" alt="Image du stage" class="col-6 d-block mx-auto my-4">
-                <p id="descriptionStage"><strong>Description :</strong> <?php echo $stage[0]['description']; ?></p>
+                <img src="<?php echo $stage[0]['miniature']; ?>" alt="Image du stage" class="col-6 mx-auto my-4">
+                <p class="col-6 text-center mx-auto my-4"><strong>Description :</strong> <?php echo $stage[0]['description']; ?></p>
                 <div class="text-center">
                     <a href="" class="btn btn-yellow px-4 py-2">S'INSCRIRE AU STAGE ></a>
                 </div>
@@ -90,8 +90,8 @@ include("ressources/ressourcesCommunes.php");
             <!-- Les animateurs -->
 
             <section>
-                <h2 class="text-center mb-4 h2-custom">LES ANIMATEURS</h2>
-                <div id="animateurs" class="row g-3">
+                <h2 class="text-center mb-4 colorB">LES ANIMATEURS</h2>
+                <div class="row gap-3 justify-content-center">
                     <?php
                     $nbAnimateur=count($stage);
                     for ($i = 0; $i < $nbAnimateur; $i++) {
@@ -107,11 +107,11 @@ include("ressources/ressourcesCommunes.php");
             <!-- Affichage de 3 stages aléatoires -->
 
             <section class="mt-5">
-                <h2 class="text-center mb-4 h2-custom">D'AUTRES STAGES</h2>
-                <div id="autresStages" class="row g-4">
+                <h2 class="text-center mb-4 colorB">D'AUTRES STAGES</h2>
+                <div class="row justify-content-center gap-4">
                     <?php
                     for ($i=0;$i<3;$i++) {
-                        echo "<div class='col-md-4'>";
+                        echo "<div class='col-lg-3 col-12'>";
                         echo "<div class='card shadow-sm h-100'>"; 
                         echo "<img src=".$stagesAleatoires[$i]['miniature']." alt='Image du stage' class='card-img-top' style='height: 200px; object-fit: cover;'>"; 
                         echo "<div class='card-body d-flex flex-column'>";
--- www/index.php
+++ www/index.php
@@ -26,7 +26,7 @@
                     <div class="carousel-item active">
                         <img src="https://www.zupimages.net/up/24/51/od2a.jpg" class="d-block w-100 img-fluid" alt="Bannière 1">
                         <div class="carousel-caption bg-dark bg-opacity-50 rounded p-3 d-flex flex-column justify-content-center">
-                            <h2 class="text-uppercase fw-bold text-warning">Vacances d'automne 2025 - Théâtre d'improvisation</h2>
+                            <h2 class="text-uppercase text-warning">Vacances d'automne 2025 - Théâtre d'improvisation</h2>
                             <p class="lead d-none d-xl-block">Plonge dans l'univers captivant du théâtre improvisé cet automne et libère ta créativité sur scène. Découvre des jeux scéniques drôles, dynamiques et pleins de surprises durant ce stage unique !</p>
                             <a href="details_stage.php?id=1" class="btn btn-warning fw-bold px-4 py-2">DÉCOUVRIR LE STAGE</a>
                         </div>
@@ -35,7 +35,7 @@
                 <div class="carousel-item">
                     <img src="https://www.zupimages.net/up/24/51/qwd4.jpg" class="d-block w-100 img-fluid" alt="Bannière 2">
                     <div class="carousel-caption bg-dark bg-opacity-50 rounded p-3 d-flex flex-column justify-content-center">
-                        <h2 class="text-uppercase fw-bold text-warning">Vacances d'automne 2025 - Découverte de la sérigraphie</h2>
+                        <h2 class="text-uppercase text-warning">Vacances d'automne 2025 - Découverte de la sérigraphie</h2>
                         <p class="lead d-none d-xl-block">Découvre l'art fascinant de la sérigraphie cet automne et crée des œuvres uniques avec tes propres mains ! Laisse libre cours à ton imagination lors de ce stage créatif !</p>
                         <a href="details_stage.php?id=3" class="btn btn-warning fw-bold px-4 py-2"><button value="3" type="button" class="btn-yellow">DÉCOUVRIR LE STAGE</button></a>
                     </div>
@@ -43,7 +43,7 @@
                 <div class="carousel-item">
                     <img src="https://www.zupimages.net/up/24/51/h4zo.png" class="d-block w-100 img-fluid" alt="Bannière 3">
                     <div class="carousel-caption bg-dark bg-opacity-50 rounded p-3 d-flex flex-column justify-content-center">
-                        <h2 class="text-uppercase fw-bold text-warning">Vacances d'été 2025 - Cuisine</h2>
+                        <h2 class="text-uppercase text-warning">Vacances d'été 2025 - Cuisine</h2>
                         <p class="lead d-none d-xl-block">La Ka'fête ô mômes ouvre ses portes aux gourmands ! Cet été, mets la main à la pâte et explore l'art de la cuisine dans un stage gourmand et plein de saveurs ! Rejoins nous vite !</p>
                         <a href="details_stage.php?id=4" class="btn btn-warning fw-bold px-4 py-2"><button value="4" type="button" class="btn-yellow">DÉCOUVRIR LE STAGE</button></a>
                     </div>
@@ -62,35 +62,35 @@
             
         <!-- Section 1-adjectifs -->
         <section class="container my-5">
-            <div class="row g-4">
-                    <div class="col-md-6 d-flex align-items-center text-center">
+            <div class="row gap-4 justify-content-center">
+                    <div class="col-lg-5 col-12 d-flex align-items-center text-center">
                         <img src="img/encadre.png" alt="Icône montrant un groupe d'adultes" class="img-fluid me-3" style="width: 100px; height: 100px; border-radius: 50%; object-fit: cover;">
                         <div>
-                            <h4>Encadré</h4>
+                            <h4 class="text-start">Encadré</h4>
                             <p>Nos stages sont dirigés par des professionnels qualifiés, assurant un suivi personnalisé et un accompagnement de qualité pour vos enfants !</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 d-flex align-items-center text-center">
+                    <div class="col-lg-5 col-12 d-flex align-items-center text-center">
                         <img src="img/pedagogique.png" alt="Icône montrant une ampoule" class="img-fluid me-3" style="width: 100px; height: 100px; border-radius: 50%; object-fit: cover;">
                         <div>
-                            <h4>Pédagogique</h4>
+                            <h4 class="text-start">Pédagogique</h4>
                             <p>Chaque activité est conçue pour stimuler la créativité et l'apprentissage des enfants, tout en respectant leur rythme et leurs besoins.</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 d-flex align-items-center text-center">
+                    <div class="col-lg-5 col-12 d-flex align-items-center text-center">
                         <img src="img/securise.png" alt="Icône montrant un bouclier" class="img-fluid me-3" style="width: 100px; height: 100px; border-radius: 50%; object-fit: cover;">
                         <div>
-                            <h4>Sécurisé</h4>
+                            <h4 class="text-start">Sécurisé</h4>
                             <p>La sécurité de vos enfants est notre priorité. Nos lieux d'activités sont sécurisés et nous veillons à une surveillance constante pour garantir leur bien-être.</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 d-flex align-items-center text-center">
+                    <div class="col-lg-5 col-12 d-flex align-items-center text-center">
                         <img src="img/ludique.png" alt="Icône montrant un ballon" class="img-fluid me-3" style="width: 100px; height: 100px; border-radius: 50%; object-fit: cover;">
                         <div>
-                            <h4>Ludique</h4>
+                            <h4 class="text-start">Ludique</h4>
                             <p>Les stages sont avant tout des moments de plaisir, où les enfants apprennent en s'amusant, dans une ambiance dynamique et conviviale.</p>
                         </div>
                     </div>
@@ -100,15 +100,15 @@
             <!-- Section 2-stages -->
 
             <section class="container my-5">
-                <div class="row align-items-center">
-                    <div class="col-md-6 text-start">
-                        <h2 class="fw-bold text-stage mb-4">Les stages des vacances scolaires</h2>                
+                <div class="row align-items-center flex-row-reverse">
+                    <div class="col-lg-6 col-12 text-center">
+                        <img src=https://www.zupimages.net/up/24/51/4l41.png alt="Image montrant des enfants qui s'amusent" class="img-fluid rounded-4 img-custom">
+                    </div>
+                    <div class="col-lg-6 col-12 text-start">
+                        <h2 class="colorB mb-4">Les stages des vacances scolaires</h2>                
                         <p class="mb-3">Nos stages de vacances scolaires offrent à vos enfants une expérience unique alliant créativité, apprentissage et plaisir ! Animés par des professionnels passionnés, ces ateliers permettent aux jeunes de découvrir des activités ludiques et pédagogiques, telles que le théâtre ou encore la sérigraphie.</p>
                         <p class="mb-4">Chaque stage est conçu pour être sécurisé, épanouissant et adapté à l'âge de vos bouts de chou, leur offrant ainsi un cadre idéal pour grandir tout en s'amusant.</p>
-                        <a href="nos_stages.php"><button class="btn btn-warning fw-bold px-4 py-2">DÉCOUVRIR NOS STAGES ></button></a>
-                    </div>
-                    <div class="col-md-6 text-center">
-                        <img src=https://www.zupimages.net/up/24/51/4l41.png alt="Image montrant des enfants qui s'amusent" class="img-fluid img-custom">
+                        <a href="nos_stages.php"><button class="btn-warning fw-bold px-4 py-2 mb-4">DÉCOUVRIR NOS STAGES ></button></a>
                     </div>
                 </div>
             </section>
@@ -117,11 +117,11 @@
 
             <section class="container my-5">
                 <div class="row align-items-center">
-                    <div class="col-md-6 text-center">
-                        <img src=https://www.zupimages.net/up/24/51/zsi6.png alt="Image montrant notre équipe" class="img-fluid img-custom">
+                    <div class="col-lg-6 col-12 text-center">
+                        <img src=https://www.zupimages.net/up/24/51/zsi6.png alt="Image montrant notre équipe" class="img-fluid rounded-4 img-custom">
                     </div>
-                    <div class="col-md-6 text-start">
-                        <h2 class="fw-bold text-stage mb-4">L'équipe de la Ka'fête</h2>                
+                    <div class="col-lg-6 col-12 text-start">
+                        <h2 class="colorB mb-4">L'équipe de la Ka'fête</h2>                
                         <p class="mb-3">Notre équipe d'animateurs est avant tout une grande famille de passionnés qui met un point d'honneur à créer une atmosphère bienveillante. Chaque membre est spécialement formé pour accompagner les enfants dans leur développement tout en favorisant leur créativité et leur épanouissement. Chacun apporte sa pierre personnelle à l'édifice Ka'fête ô mômes !</p>
                         <a href="nos_animateurs.php"><button class="btn btn-warning fw-bold px-4 py-2">DECOUVRIR NOS ANIMATEURS ></button></a>
                     </div>
@@ -131,7 +131,7 @@
             <!-- Section 4-Nous contacter -->
 
             <section class="container text-center my-5">
-                <h2 class="text-stage mb-3">NOUS CONTACTER</h2>
+                <h2 class="colorB mb-3">NOUS CONTACTER</h2>
                 <img src="img/barre_separation.png" alt="Barre de séparation" class="img-fluid mb-4" style="max-width: 150px;">
 
                 <div class="row justify-content-center gy-4">
@@ -141,7 +141,7 @@
                         <div class="mb-2">
                             <i class="colorR bi bi-envelope-fill" style="font-size: 50px;"></i>
                         </div>
-                        <h4 class="fw-bold">E-mail</h4>
+                        <h4>E-mail</h4>
                         <p class="text-muted">kafeteomomes@gmail.com</p>
                     </div>
                     <!-- Téléphone -->
@@ -150,7 +150,7 @@
                         <div class="mb-2">
                             <i class="colorR bi bi-telephone-fill" style="font-size: 50px;"></i>
                         </div>
-                        <h4 class="fw-bold">Téléphone</h4>
+                        <h4>Téléphone</h4>
                         <p class="text-muted">04.78.61.21.79</p>
                     </div>
                     <!-- Adresse -->
@@ -161,7 +161,7 @@
                         <div class="mb-2">
                             <i class="colorR bi bi-geo-alt-fill" style="font-size: 50px;"></i>
                         </div>
-                        <h4 class="fw-bold">Adresse</h4>
+                        <h4>Adresse</h4>
                         <p class="text-muted">53 Montée de la Grande Côte<br>69001, Lyon</p>
                     </div>
                 </div>
